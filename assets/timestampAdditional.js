73b778f7eb667419afc7e10d91276d06f1f78932
Allison Driver <allisondriver@Allisons-MacBook-Pro.local>
2018-03-22 19:40:22 -0500

    added js and updated html

---
+++ assets/timestampAdditional.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+$.fx.speeds.xslow = 3000;
+
+function open(){
+	$("nav").hide();
+	$("head").hide();
+	$("body").hide();
+	$("nav").fadeIn("xslow");
+	$("head").fadeIn("xslow");
+	$("body").fadeIn("xslow");
+	$(".entry").tooltip();
+	$(".about").tooltip();
+	$("logout").tooltip();
+	$(".submitBtn").tooltip();
+}
+
+open();
+
+ function getStylesheet() {
+      var currentTime = new Date().getHours();
+
+      if (0 <= currentTime && currentTime < 5) {
+       $("head").append("<link rel='stylesheet' href='night.css' type='text/css'>");
+      }
+      if (5 <= currentTime && currentTime < 11) {
+       $("head").append("<link rel='stylesheet' href='morning.css' type='text/css'>");
+      }
+      if (11 <= currentTime && currentTime < 16) {
+       $("head").append("<link rel='stylesheet' href='daytime.css' type='text/css'>");
+      }
+      if (16 <= currentTime && currentTime < 22) {
+       $("head").append("<link rel='stylesheet' href='dusk.css' type='text/css'>");
+      }
+      if (22 <= currentTime && currentTime <= 24) {
+       $("head").append("<link rel='stylesheet' href='night.css' type='text/css'>");
+      }
+}
+
+getStylesheet();
+
+
+
+
+
+
+
+});
